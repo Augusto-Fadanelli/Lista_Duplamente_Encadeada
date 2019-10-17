678f74491f4d021db660ec5ac92cff04fa86df37
Augusto Fadanelli de Sousa <32231958+Augusto-Fadanelli@users.noreply.github.com>
2019-10-17 17:25:45 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,6 +25,11 @@ typedef struct no{
 
 int menu();
 
+void insert_inHead(no **);
+void insert_inTail(no *, no **);
+
+void toList_atHead(no *, no **);
+
 void clear();
 void pause();
 
@@ -33,29 +38,37 @@ int main()
     setlocale(LC_ALL, "Portuguese");
 
     no *head = NULL;
-    no *tail = NULL;
+    //no *tail = NULL;
 
     int flag = 0;
 
     do{
         switch(menu()){
         case 1:
-        //Inserir
+        //Inserir no início
+            insert_inHead(&head);
 
         break;
         case 2:
-        //Inserir por posição
+        //Inserir no final
+            insert_inTail(head, &head);
+
         break;
         case 3:
-        //Excluir
+        //Inserir por posição
         break;
         case 4:
-        //Listar como lista
+        //Excluir
         break;
         case 5:
-        //Listar como pilha
+        //Listar como lista
+            toList_atHead(head, &head);
+            pause();
         break;
         case 6:
+        //Listar como pilha
+        break;
+        case 7:
         //Sair
             flag = 1;
         break;
@@ -71,16 +84,72 @@ int main()
     return 0;
 }
 
+void insert_inHead(no **head)
+{
+    int value;
+    cout << "Digite o inteiro a ser inserido: ";
+    cin >> value;
+
+    no *aux = (no *)malloc(sizeof(no));
+
+    aux->value = value;
+    aux->next = *head;
+    aux->prev = NULL;
+
+    /*if(*head == NULL){
+        *tail = aux;
+    }*/
+
+    *head = aux;
+}
+
+void insert_inTail(no *aux, no **head)
+{
+    int value;
+    cout << "Digite o inteiro a ser inserido: ";
+    cin >> value;
+
+    no *novo = (no *)malloc(sizeof(no));
+
+    if(*head != NULL)
+    while(aux->next != NULL){
+        //aux percorre até o final
+    }
+
+
+    novo->value = value;
+    novo->next = NULL;
+    novo->prev = aux;
+}
+
+void toList_atHead(no *aux, no **head)
+{
+    if(*head = NULL){
+        cout << "Lista Vazia!" << endl;
+        pause();
+    }else{
+        int cont = 0;
+        do{
+            cont++;
+            cout << "[" << cont << "º] " << aux->value << " | ";
+            aux = aux->next;
+        }while(aux != NULL);
+    }
+
+
+}
+
 int menu(){
     int op;
 
     cout << "***LISTA DUPLAMENTE ENCADEADA***" << endl;
-    cout << "[1] - Inserir" << endl;
-    cout << "[2] - Inserir por Posição" << endl;
-    cout << "[3] - Excluir" << endl;
-    cout << "[4] - Listar como Lista" << endl;
-    cout << "[5] - Listar como Pilha" << endl;
-    cout << "[6] - Sair" << endl;
+    cout << "[1] - Inserir no Início" << endl;
+    cout << "[2] - Inserir no Final" << endl;
+    cout << "[3] - Inserir por Posição" << endl;
+    cout << "[4] - Excluir" << endl;
+    cout << "[5] - Listar do Início" << endl;
+    cout << "[6] - Listar do Final" << endl;
+    cout << "[7] - Sair" << endl;
     cin >> op;
 
     return op;
